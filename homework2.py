4f65f8441edc8fb60fc6180a9fac75f46a89f373
Tregub-16 <vovan_tregub@mail.ru>
2024-07-22 10:39:03 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,4 @@
+The_number_of_completed_DZ = 12
+Number_of_hours_spent = 1.5
+Course_name = 'Python'
+print(Course_name, The_number_of_completed_DZ,Number_of_hours_spent,Number_of_hours_spent / The_number_of_completed_DZ )
